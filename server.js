f362434eb1c8d777257e44045433c80b06a30071
AditiVashisht20 <71002267+AditiVashisht20@users.noreply.github.com>
2022-03-02 12:39:37 +0530

    Added body parser

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express();
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 var port = 5000
